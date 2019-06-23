e1c435c1f261d6e9ba6602cd6681423bb7fc9959
therour <nizomsidiq@gmail.com>
2019-06-23 16:53:31 +0700

    Add compileErrorbag
    error blade directive with bag defined.

--- Compilers/Concerns/CompilesErrors.php
+++ Compilers/Concerns/CompilesErrors.php
@@ -31,4 +31,32 @@ $message = $errors->first('.$expression.'); ?>';
 if (isset($messageCache)) { $message = $messageCache; }
 endif; ?>';
     }
+    
+    /**
+     * Compile the errorbag statement into valid PHP.
+     *
+     * @param string $expression
+     * @return string
+     */
+    protected function compileErrorbag($expression)
+    {
+        $expression = explode(',', $this->stripParentheses($expression));
+        $bag = Arr::get($expression, 0);
+        $attribute = trim(Arr::get($expression, 1));
+        
+        return '<?php if ($errors->getBag('.$bag.')->has('.$attribute.')) :
+if (isset($message)) { $messageCache = $message; }
+$message = $errors->getBag('.$bag.')->first('.$attribute.'); ?>';
+    }
+
+    /**
+     * Compile the enderrorbag statements into valid PHP.
+     *
+     * @param  string  $expression
+     * @return string
+     */
+    protected function compileEnderrorbag($expression)
+    {
+        return $this->compileEnderror($expression);
+    }
 }
